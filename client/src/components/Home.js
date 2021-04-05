b297bb24ea9c0a8e6a401d69fe21170f826d6f64
Chirag Arora <carora755@gmail.com>
2021-04-05 14:44:07 +0530

    back to basics

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/home.scss";
+import { Button } from "semantic-ui-react";
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.joinRoom = this.joinRoom.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+    this.inputChange = this.inputChange.bind(this);
     this.state = {
       inputValue: "",
     };
@@ -15,53 +17,43 @@ class Home extends React.Component {
     console.log(this.props);
   }
 
+  inputChange = (e) => {
+    this.setState({inputValue: e.target.value})
+  }
+
   joinRoom = () => {
+    console.log('in jr')
+    this.props.socket.emit("joinRoom", {
+      roomHash: this.state.inputValue,
+    });
+  };
+  createRoom = () => {
+    console.log('in jr')
     this.props.socket.emit("joinRoom", {
       roomHash: this.props.roomHashDuo,
     });
   };
 
   render() {
-    const inputChange = (e) => {
-      console.log(e);
-      this.setState({
-        inputValue: e.target.value,
-      });
-    };
-
     return (
-      <div className="home page">
-        <div className="main">
-          <div className="ui huge header inverted">
-            <span className="span">Code Wars</span>
-          </div>
-          <div className="ui hidden divider"></div>
-          <div className="input">
-            <form action={`/code/solo/${this.state.inputValue}`}>
-              <input
-                type="text"
-                placeholder="Join Room"
-                name="room"
-                onChange={inputChange}
-              />
-              <Link
-                to={`/code/duo/${this.state.inputValue}`}
-                className="ui button big inverted"
-                onClick={this.joinRoom}
-                id="button"
-              >
-                Duo
-              </Link>
-              <Link
-                to={`/code/solo/${this.state.inputValue}`}
-                className="ui button big inverted"
-                id="button"
-              >
-                Solo
-              </Link>
-            </form>
-          </div>
-        </div>
+      <div>
+        <h1>Home Page</h1>
+        <input type="text" onChange={this.inputChange}/>
+        <Button onClick={this.joinRoom}>
+          <Link to={`/code/duo/${this.props.roomHashDuo}`}>
+          Duo Join
+        </Link>
+        </Button>
+        <Button onClick={this.createRoom}>
+          <Link to={`/code/duo/${this.props.roomHashDuo}`}>
+          Duo Create
+        </Link>
+        </Button>
+        <Button>
+          <Link to={`/code/solo/${this.props.roomHashSolo}`}>
+          Solo
+        </Link>
+        </Button>
       </div>
     );
   }
