9ea0c4d454e80f8825f921a2b244de862e7f1d34
Chirag Arora <carora755@gmail.com>
2021-04-11 16:43:24 +0530

    one step ahead of basics

--- client/src/components/AppDuo.js
+++ client/src/components/AppDuo.js
@@ -1,9 +1,18 @@
-import React, { useState, useEffect } from "react"; 
-import { Button } from 'semantic-ui-react';
+import React, { useState, useEffect } from "react";
+import { Dropdown } from "semantic-ui-react";
+
 import languages from "../utils/languages";
+import Editor from './AceEditor';
 import "../styles/AppDuo.scss";
 //editor
-import Editor from "./AceEditor";
+import AceEditor from "react-ace";
+import * as ace from "ace-builds";
+import RunButton from "./button";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+import { Autocomplete } from "ace-builds/src-noconflict/ext-language_tools";
 
 function getWindowDimensions() {
   const { innerWidth: width } = window;
@@ -14,49 +23,90 @@ function getWindowDimensions() {
 
 const AppDuo = ({ socket }) => {
   const { width } = getWindowDimensions();
-  const [lang, setLang] = useState(languages[0]);
-  const [viewOpponent, setViewOpponent] = useState(false);
+  const [currentLang1, setCurrentLang1] = useState(languages[0].key);
+  const [codeValue1, setCodeValue1] = useState(languages[0].template);
+  const [currentLang2, setCurrentLang2] = useState(languages[0].key);
+  const [codeValue2, setCodeValue2] = useState(languages[0].template);
+  const [resizerPos, setResizerPos] = useState(`${0.5 * width}`);
   const [editor1, setEditor1] = useState(false);
   const [editor2, setEditor2] = useState(false);
-  const [spectator, setSpectator] = useState(true);
+  const [spectator, setSpectator] = useState(false);
+
+  const dragEnd = (e) => {
+    const posi = Math.min(0.7 * width, Math.max(0.3 * width, e.clientX));
+    setResizerPos(posi);
+    // console.log(e.clientX);
+  };
+
+  const onDDChange1 = (e, data) => {
+    const selectedVal = languages.filter((v) => v.key === data.value);
+    setCurrentLang1(data.value);
+    setCodeValue1(selectedVal[0].template);
+  };
+
+  const onDDChange2 = (e, data) => {
+    const selectedVal = languages.filter((v) => v.key === data.value);
+    setCurrentLang2(data.value);
+    setCodeValue2(selectedVal[0].template);
+  };
 
   useEffect(() => {
-    console.log("useEffect")
-    socket.on("allow1", () => {
-      console.log("user 1 allowed");
-      setEditor1(true);
-      setSpectator(false);
+    socket.on("allow", (data) => {
+      console.log('in allow')
+      if (data.data.members[0] === socket.id) {
+        console.log(1)
+        setEditor1(true);
+      } else if (data.data.members[1] === socket.id) {
+        console.log(2);
+        setEditor2(true);
+      } else {
+        setSpectator(true);
+      }
     });
-  }, []);
-  useEffect(() => {
-    socket.on("allow2", () => {
-      console.log("user 2 allowed");
-      setEditor2(true);
-      setSpectator(false);
-    });
-  },[]);
+  });
 
   return (
     <div className="row">
-      <div className="modal" style={viewOpponent ? { display: "block" } : { display: "none" }}  onClick={()=>setViewOpponent(false)}/>
-      <div className="opponentInfoContainer" style={viewOpponent?{transform: "translateX(0px)"}:{transform: "translateX(400px)"}}>
-        <Button onClick={()=>setViewOpponent(!viewOpponent)} style={{height:"40px"}}>view info</Button>
-        <div className="opponentInfo"></div>
-      </div>
-      <div className="langContainer">
-        <div className="langBtn">{lang.key}</div>
-          <div className="langItem item1" onClick={()=>setLang(languages[0])}>C++</div>
-          <div className="langItem item2" onClick={()=>setLang(languages[1])}>C</div>
-          <div className="langItem item3" onClick={()=>setLang(languages[2])}>Java</div>
-          <div className="langItem item4" onClick={()=>setLang(languages[3])}>Python 3</div>
-      </div>
-      <div style={{display:`${editor1||spectator?'block':'none'}`}}>
-      <Editor spectator={spectator} editor1={true} editor2={false} codeValue={lang.template} modeIndex={lang.index} currentLang={lang.key} socket={socket}/>
-      </div>
-      <div style={{display:`${editor2||spectator?'block':'none'}`}}>
-        <Editor spectator={spectator} editor1={false} editor2={true} codeValue={lang.template} modeIndex={lang.index} currentLang={lang.key} socket={socket}/>
-      </div>
-		</div>
-	);
+      <div style={{ width: `${resizerPos}px` }}>
+        <Dropdown
+          style={{
+            margin: "1rem 1rem 0rem 1rem",
+            borderRadius: "1rem",
+          }}
+          placeholder="Languages"
+          onChange={onDDChange1}
+          selection
+          value={currentLang1}
+          options={languages}
+        />
+
+        <Editor spectator ={spectator} editor={editor2} codeValue={codeValue1} socket={socket}/>
+        
+        </div>
+      <div
+        className="resizer"
+        style={{ left: `${resizerPos}px` }}
+        draggable="true"
+        onDragEnd={dragEnd}
+      ></div>
+      <div
+        style={{ width: `${width - resizerPos - 10}px`, marginLeft: "10px" }}
+      >
+        <Dropdown
+          style={{
+            margin: "1rem 1rem 0rem 1rem",
+            borderRadius: "1rem",
+          }}
+          placeholder="Languages"
+          onChange={onDDChange2}
+          selection
+          value={currentLang2}
+          options={languages}
+        />
+        <Editor spectator ={spectator} editor={editor1} codeValue={codeValue2} socket ={socket}/>
+        
+        </div>
+    </div>
+  );
 };
 export default AppDuo;
